7d1b554e621d90cc1b2fdb4f84a0c454cd31db59
connorgionet <connorgionet08@gmail.com>
2025-11-18 18:41:38 -0500

    merged changes

--- calculator.py
+++ calculator.py
@@ -1,6 +1,15 @@
 
 import math
 
+def square_root(a):
+    if a < 0:
+        raise ValueError("must be non-negative number")
+    return math.sqrt(a)
+
+
+def hypotenuse(a, b):
+    return math.hypot(a,b)
+
 def add(a, b):
     return a + b
 
@@ -26,31 +35,7 @@ def exp(a,b):
     return a**b
 
 
-def square_root(a):
-    if a < 0:
-        raise ValueError("must be non-negative number")
-    return math.sqrt(a)
 
 
-def hypotenuse(a, b):
-    return math.hypot(a,b)
-
-
-def add(a, b):
-    return a + b
-
-def subtract(a, b):
-    return a - b
-
-def multiply(a, b):
-    return a * b
-
-def logarithm(a, b):
-    if a <= 0 or a == 1 or b <= 0:
-        raise ValueError("Invalid values for logarithm.")
-    return math.log(b, a)
-
-def exponent(a, b):
-    return a ** b
 
 
